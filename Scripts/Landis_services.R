ff66267b6d3b85588d5ea9e8e8ecb3756b18f023
Phil Martin <philip.martin.1981@gmail.com>
2015-09-24 10:45:51 +0100

    added script to produce service estimates from Landis using observations from Paul's plots

---
+++ Scripts/Landis_services.R
@@ -0,0 +1,20 @@
+#script to produce maps of biodiversity, ecosystem function and ecosystem services
+#based on Paul's gradient plots
+
+#author: Phil martin
+#Date 2015/09/24
+
+#open packages
+library(raster)
+library(ggplot2)
+library(reshape2)
+library(plyr)
+
+
+
+#load data Landis II outputs
+raster("Century_outputs/")
+
+File_names<-list.files(pattern="*.img",recursive=T)
+File_names<-File_names[!grepl("*.dbf",File_names)]
+File_names<-File_names[grepl("TotalBiomass",File_names)]
