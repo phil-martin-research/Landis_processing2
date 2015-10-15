436e7bfc56dcea5f64bfc1440111a09fcaa3c5c0
Phil Martin <phil.martin.research@gmail.com>
2015-10-15 11:23:00 +0100

    edited carbon and nitrogen flux script to give appropriate output

--- Scripts/C_N_flux.R
+++ Scripts/C_N_flux.R
@@ -37,15 +37,17 @@ for (i in 1:length(C_N)){
   #
   File_sub3$C_change<-NA
   File_sub3$N_change<-NA
-
-  File_sub3<-File_sub3[with(File_sub3, order(EcoregionIndex, Time)), ]
-  for (j in 2:nrow(File_sub3)){
-    head(File_sub3)
-    File_sub3$C_change[j]<-File_sub3$Total_C[j]-File_sub3$Total_C[j-1]
-    File_sub3$N_change[j]<-File_sub3$TotalN[j]-File_sub3$TotalN[j-1]
+  File_sub3<-File_sub3[with(File_sub3, order(Time)), ]
+  UN_ER<-unique(File_sub3$EcoregionName)
+  
+  for (j in 1:length(UN_ER)){
+    ER_sub<-subset(File_sub3,EcoregionName==UN_ER[j])
+    for (k in 2:nrow(ER_sub)){
+      ER_sub$C_change[k]<-ER_sub$Total_C[k]-ER_sub$Total_C[k-1]
+      ER_sub$N_change[k]<-ER_sub$TotalN[k]-ER_sub$TotalN[k-1]  
   }
-  #bind all these outputs together
-  CN_ER<-rbind(File_sub3,CN_ER)
+  CN_ER<-rbind(ER_sub,CN_ER)
+  }
 }
 
 head(CN_ER)
@@ -56,15 +58,15 @@ WM_CN<-ddply(CN_ER_sum,.(Scenario,Time),summarise,Mean_C=weighted.mean(m_c,Num_s
 
 #make plot of carbon change
 theme_set(theme_bw(base_size=12))
-P1<-ggplot(CN_ER_sum,aes(x=Time,y=m_c,group=EcoregionName))+geom_line(alpha=0.2)+facet_wrap(~Scenario)
+P1<-ggplot(CN_ER_sum,aes(x=Time,y=m_c,group=EcoregionName))+geom_line(alpha=0.2)+facet_wrap(~Scenario,scales = "free_y")
 P2<-P1+geom_ribbon(data=WM_CN,aes(y=Mean_C,ymax=Mean_C+SD_C,ymin=Mean_C-SD_C,group=NULL),alpha=0.5)+geom_line(data=WM_CN,aes(y=Mean_C,group=NULL),size=2,alpha=0.8)
 P3<-P2+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))
-P3+ylab(expression(paste("Carbon flux (Mg",~Ha^-1,")")))+xlab("Time (Years)")+theme(panel.margin = unit(1, "lines"))
+P3+ylab(expression(paste("Carbon flux (Mg",~Ha^-1,")")))+xlab("Time (Years)")
 ggsave("Figures/Carbon_flux.pdf",dpi = 400,height=6,width=8,units="in")
 
 #make plot of nitrogen change
 theme_set(theme_bw(base_size=12))
-P1<-ggplot(CN_ER_sum,aes(x=Time,y=m_n,group=EcoregionName))+geom_line(alpha=0.2)+facet_wrap(~Scenario)
+P1<-ggplot(CN_ER_sum,aes(x=Time,y=m_n,group=EcoregionName))+geom_line(alpha=0.2)+facet_wrap(~Scenario,scales="free_y")
 P2<-P1+geom_ribbon(data=WM_CN,aes(y=Mean_N,ymax=Mean_N+SD_N,ymin=Mean_N-SD_N,group=NULL),alpha=0.5)+geom_line(data=WM_CN,aes(y=Mean_N,group=NULL),size=2,alpha=0.8)
 P3<-P2+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))
 P3+ylab(expression(paste("Nitrogen flux")))+xlab("Time (Years)")+theme(panel.margin = unit(1, "lines"))
