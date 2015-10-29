f13ccafcabf2c1cc0d26e8aeddfa3b2b12d1262d
Phil Martin <phil.martin.research@gmail.com>
2015-10-29 14:09:13 +0000

    added regression for commercially valuable fungi species richness

--- Scripts/Biomass_regression.R
+++ Scripts/Biomass_regression.R
@@ -42,7 +42,7 @@ summary(BM$AGB_std)
 df<-NULL
 df<-data.frame(AGB_std=seq(-1.3,3.0,0.01))
 df$AGB<-(df$AGB_std*sd(BM$AGB))+mean(BM$AGB)
-df<-data.frame(df,SRR=NA,Min_rate=NA,Fungi=NA,GF=NA,Lichen=NA)
+df<-data.frame(df,SRR=NA,Min_rate=NA,Fungi=NA,GF=NA,Lichen=NA,Val_fungi=NA)
 Coefficients<-NULL
 for (i in 4:5){
   M1<-lmer(BM[[i]]~AGB_std+(1|Site),data=BM)
@@ -56,7 +56,7 @@ for (i in 4:5){
                                  AGB=coef(Mod_average)[2],
                                  AGB_sq=coef(Mod_average)[3]))
 }
-for (i in 6:8){
+for (i in 6:9){
   M1<-glmer(BM[[i]]~AGB_std+(1|Site),data=BM,family="poisson")
   M2<-glmer(BM[[i]]~AGB_std+I(AGB_std^2)+(1|Site),data=BM,family="poisson")
   M0<-glmer(BM[[i]]~1+(1|Site),data=BM,family="poisson")
@@ -69,6 +69,7 @@ for (i in 6:8){
                                  AGB_sq=coef(Mod_average)[3]))
 }
 
+head(df)
 #tidy data for plotting
 rownames(Coefficients)<-NULL
 BM_melt<-melt(BM,id.vars = c("Site","Plot","AGB_std","AGB"))
@@ -77,9 +78,11 @@ df_melt<-melt(df,id.vars = c("AGB_std","AGB"))
 
 #now plot all predictions
 theme_set(theme_bw(base_size=12))
-P1<-ggplot(BM_melt,aes(x=AGB,y=value,group=Site))+geom_point(size=3,shape=1)+facet_wrap(~variable,scales = "free_y")
-P1+geom_line(data=df_melt,aes(group=NULL),size=2)+
+P1<-ggplot(BM_melt,aes(x=AGB,y=value,group=Site))+geom_point(size=3,shape=1)+facet_wrap(~variable,scales = "free")
+P2<-P1+geom_line(data=df_melt,aes(group=NULL),size=2)+
   theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))                                                                                                                                           
+P2+annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf)+
+  annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf)
 
 ggsave("Figures/AGB_regressions.pdf",dpi=400,height=6,width=8,units='in')
 
