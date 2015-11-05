8d78991b868f08d2320b0d1e9fccc8bb47b91ae7
Phil Martin <phil.martin.research@gmail.com>
2015-11-05 16:56:44 +0000

    modified ecoregions script and trajectories plot so that previous errors have been fixed

---
+++ Scripts/Ecoregion_trajectories.R
@@ -0,0 +1,48 @@
+library(ggplot2)
+library(gridExtra)
+library(grid)
+
+Eco_summary<-read.csv("Data/R_output/Ecoregion_summary.csv")
+Eco_means<-read.csv("Data/R_output/Ecoregion_means.csv")
+
+head(Eco_means)
+str(Eco_summary)
+
+Eco_means<-Eco_means[,-16]
+
+Eco_means$Scenario<-factor(Eco_means$Scenario,c("Scenario 1","Scenario 2","Scenario 3","Scenario 4","Scenario 5","Scenario 6", "Scenario 12"))
+Eco_summary$Scenario<-factor(Eco_summary$Scenario,c("Scenario 1","Scenario 2","Scenario 3","Scenario 4","Scenario 5","Scenario 6", "Scenario 12"))
+
+out<-NULL
+t<-1
+theme_set(theme_bw(base_size=12))
+for (i in names(Eco_means)[-c(1:6)]){
+  Y_var<-i
+  Y_var2<-paste(i,"_M",sep = "")
+  Var_SD<-paste(i,"_SD",sep = "")
+  P1<-ggplot(Eco_means,aes_string(x="Time",y=Y_var,group="EcoregionIndex"))+geom_line(size=0.5,alpha=0.2)+facet_wrap(~Scenario,nrow=1,scales="free")
+  P2<-P1+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))
+  P3<-P2+geom_line(data=Eco_summary,aes_string(x="Time",y=Y_var2,group=NULL),size=1.1) +theme(plot.margin = unit(c(0,0.5,0,0), "line"))
+  out[[t]]<-P3
+  t<-t+1
+}
+
+
+pdf(file = "Figures/Ecosystem_trajectories_1.pdf",width = 10,height=8)
+do.call(grid.arrange, c(out[1],out[2],out[3],out[4],out[5],out[6],
+                        list(ncol=1)))
+dev.off()
+
+pdf(file = "Figures/Ecosystem_trajectories_2.pdf",width = 10,height=8)
+do.call(grid.arrange, c(out[7],out[8],out[9],out[10],out[11],out[12],
+                        list(ncol=1)))
+dev.off()
+
+
+
+theme_set(theme_bw(base_size=12))
+P1<-ggplot(Eco_means,aes(x=Time,y=Aesthetic,group=EcoregionIndex))+geom_line(alpha=0.3)+facet_wrap(~Scenario,nrow=1)
+P2<-P1+geom_line(data=Eco_summary,aes(x=Time,y=Aesthetic_M,group=NULL),size=2)+
+  geom_ribbon(data=Eco_summary,aes(y=Aesthetic_M,ymax=Aesthetic_M+Aesthetic_SD,ymin=Aesthetic_M-Aesthetic_SD,group=NULL),alpha=0.5)
+P3<-P2+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))
+P3
