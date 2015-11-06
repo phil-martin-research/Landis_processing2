2473f5fedfc1404d40a5f15568184f8130b477af
Phil Martin <phil.martin.research@gmail.com>
2015-11-06 13:57:17 +0000

    added new code to calculate recovery time

--- Scripts/Resistance_recovery.R
+++ Scripts/Resistance_recovery.R
@@ -83,9 +83,8 @@ for (i in 1:length(Un_Scen)){
   Scen_sub<-subset(Eco_summary,Scenario==Un_Scen[i])
   head(Scen_sub)
   for (j in seq(4,26,by = 2)){
-    #j<-6
+
     for (k in 7:nrow(Scen_sub)){
-      #k<-7
     Recovery<-data.frame(Scenario=unique(Un_Scen[i]),Variable=colnames(Scen_sub[j]),
                          Time=Scen_sub[k,3],
                          Resistance=1-((2*(Scen_sub[2,j]-Scen_sub[6,j]))/(Scen_sub[2,j]+(Scen_sub[2,j]-Scen_sub[6,j]))),
@@ -93,17 +92,51 @@ for (i in 1:length(Un_Scen)){
                            Recovery=(((2*(Scen_sub[2,j]-Scen_sub[6,j]))/
                                ((Scen_sub[2,j]-Scen_sub[6,j])+
                                   (Scen_sub[2,j]-Scen_sub[k,j])))-1)/
-                           (Scen_sub[k,3]-Scen_sub[6,3]))
-    Recovery_summary<-rbind(Recovery,Recovery_summary)
+                           (Scen_sub[k,3]-Scen_sub[6,3]),
+                         value=Scen_sub[k,j])
+    Recovery_summary<-rbind(Recovery_summary,Recovery)
   }
 }
-#Recovery_summary2<-rbind(Recovery_summary,Recovery_summary2)
 }
 
 Recovery_summary$Resistance<-ifelse(Recovery_summary$Scenario=="Scenario 1",1,Recovery_summary$Resistance)#set Scenario 1 resistance as equal to 1
 Recovery_summary$Resistance<-ifelse(Recovery_summary$Resistance>=1,1,Recovery_summary$Resistance) #set resistance as equal to 1 if variable increases
 
-Recovery_sub<-subset(Recovery_summary,Time>5)
+Recovery_sub<-subset(Recovery_summary,Time>5&Scenario!="Scenario 1"&Resistance<1)
 head(Recovery_sub)
 
 ggplot(Recovery_sub,aes(x=Time,y=Resistance2,colour=Scenario))+geom_line()+facet_wrap(~Variable,scales="free_y")
+
+#now work out the first time point at which Resistance2>=1, thereby working out the time
+#taken for each ecosystem service/biodiversity variable to recover
+
+#for each scenario, for each service/biodiversity metric work out the time at which resistance>=1
+Un_Sce_ES<-expand.grid(unique(Recovery_sub$Scenario),unique(Recovery_sub$Variable))
+
+R_summ<-NULL
+for (i in 1:nrow(Un_Sce_ES)){
+  Scen_sub<-subset(Recovery_sub,Scenario==Un_Sce_ES[i,1]&Variable==Un_Sce_ES[i,2])
+  First_recov<-which(Scen_sub$Resistance2>=1)[1]
+  if (!is.na(First_recov)){
+  Recov_summary<-Scen_sub[(First_recov),]
+  }else{
+   Recov_summary<-Scen_sub[(First_recov),]
+   Recov_summary$Scenario<-Un_Sce_ES[i,1]
+   Recov_summary$variable<-Un_Sce_ES[i,2]
+   Recov_summary$Time<-100
+  }
+  R_summ<-rbind(R_summ,Recov_summary)
+}
+
+R_summ$ESLab <- ES_labeller('variable',R_summ$variable)
+
+#plot of time taken for recovery
+P1<-ggplot(R_summ,aes(x=Scenario,y=Time))+geom_point()+facet_wrap(~Variable,scales="free_y",ncol=5)
+P2<-P1+theme(axis.text.x = element_text(angle = 90))+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))
+P2+ylab("Time taken for recovery (Years)")
+
+#plot of recovery rate
+P1<-ggplot(R_summ,aes(x=Scenario,y=Recovery*100))+geom_point()+facet_wrap(~Variable,scales="free_y",ncol=5)
+P2<-P1+theme(axis.text.x = element_text(angle = 90))+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.border = element_rect(size=1.5,colour="black",fill=NA))
+P2+ylab("Annual percentage increase in variable")
+
